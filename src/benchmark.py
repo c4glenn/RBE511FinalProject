ce355d19dfdb42f5596ece8e4863a884dba40079
Philip Smith <philip@randomsmiths.com>
2026-04-16 17:18:46 -0400

    upgrade benchmarks a bunch adding a save function and itterable support

--- src/benchmark.py
+++ src/benchmark.py
@@ -1,5 +1,6 @@
 #TODO: Move this file out and make it a library so linking works correctly
-from typing import Optional, Tuple, List
+from typing import Optional, Tuple, List, Dict, Any
+import datetime
 import logging
 import argparse
 from dataclasses import dataclass
@@ -7,62 +8,113 @@ import copy
 import numpy as np
 import tqdm
 import jsonpickle
-
+import mesa
+import pandas as pd
 from model import SwarmModel
 
 logger = logging.getLogger()
 
 @dataclass
 class ModelParams():
-    n_robots:int = 20
-    n_tasks:int=1
-    speed:float = 3.0
-    seed: int = 42
-    arena_width:float= 600.0
-    arena_height:float = 200.0
-    interface_gap:float = 25.0
-    task_dist_calc: int = 50
-    task_distribution: Optional[np.ndarray] = None
-    robot_initial_placements: Optional[np.ndarray] = None
+    n_robots:int | List[int] = 20
+    n_tasks:int | List[int]=1
+    speed:float | List[float] = 3.0
+    seed: Optional[int] = None
+    arena_width:float | List[float] = 600.0
+    arena_height:float | List[float] = 200.0
+    interface_gap:float | List[float] = 25.0
+    task_dist_calc: int| List[int] = 50
+    task_distribution: Optional[np.ndarray] | List[np.ndarray] = None
+    robot_initial_placements: Optional[np.ndarray] | List[np.ndarray] = None
     allowed_to_switch:bool = True
-    gamma: float = 0.1
-    k:float = 5
-    m:float = 8
-    switching_cost: int = 30
-    delay_random_range: Tuple[float, float] = (0.0, 10.0)
-    transfer_time: int = 3
-    pickup_time: int = 2
-    dropoff_time: int = 26
+    gamma: float | List[float] = 0.1
+    k:float | List[float]= 5
+    m:float | List[float]= 8
+    switching_cost: int | List[int]= 30
+    delay_random_range: Tuple[float, float] | List[Tuple[float, float]] = (0.0, 10.0)
+    transfer_time: int | List[int]= 3
+    pickup_time: int | List[int] = 1
+    dropoff_time: int | List[int] = 1        
     
+    def __post_init__(self):
+        pass
+        
     def create_swarm_model(self) -> SwarmModel:
         return SwarmModel(
-            n_robots=self.n_robots,
-            n_tasks=self.n_tasks,
-            speed=self.speed,
-            seed=self.seed,
-            arena_width=self.arena_width,
-            arena_height=self.arena_height,
-            interface_gap=self.interface_gap,
-            task_dist_calc=self.task_dist_calc,
-            task_distribution=self.task_distribution,
-            robot_initial_placements=self.robot_initial_placements,
-            allowed_to_switch=self.allowed_to_switch,
-            gamma=self.gamma,
-            k=self.k,
-            m=self.m,
-            switching_cost=self.switching_cost,
-            delay_random_range=self.delay_random_range,
-            transfer_time=self.transfer_time,
-            pickup_time=self.pickup_time,
-            dropoff_time=self.dropoff_time
+            n_robots=self.n_robots if not isinstance(self.n_robots, list) else self.n_robots[0],
+            n_tasks=self.n_tasks if not isinstance(self.n_tasks, list) else self.n_tasks[0],
+            speed=self.speed if not isinstance(self.speed, list) else self.speed[0],
+            seed=self.seed, # type: ignore
+            arena_width=self.arena_width if not isinstance(self.arena_width, list) else self.arena_width[0],
+            arena_height=self.arena_height if not isinstance(self.arena_height, list) else self.arena_height[0],
+            interface_gap=self.interface_gap if not isinstance(self.interface_gap, list) else self.interface_gap[0],
+            task_dist_calc=self.task_dist_calc if not isinstance(self.task_dist_calc, list) else self.task_dist_calc[0],
+            task_distribution=self.task_distribution if not isinstance(self.task_distribution, list) else self.task_distribution[0],
+            robot_initial_placements=self.robot_initial_placements if not isinstance(self.robot_initial_placements, list) else self.robot_initial_placements[0],
+            allowed_to_switch=self.allowed_to_switch if not isinstance(self.allowed_to_switch, list) else self.allowed_to_switch[0],
+            gamma=self.gamma if not isinstance(self.gamma, list) else self.gamma[0],
+            k=self.k if not isinstance(self.k, list) else self.k[0],
+            m=self.m if not isinstance(self.m, list) else self.m[0],
+            switching_cost=self.switching_cost if not isinstance(self.switching_cost, list) else self.switching_cost[0],
+            delay_random_range=self.delay_random_range if not isinstance(self.delay_random_range, list) else self.delay_random_range[0],
+            transfer_time=self.transfer_time if not isinstance(self.transfer_time, list) else self.transfer_time[0],
+            pickup_time=self.pickup_time if not isinstance(self.pickup_time, list) else self.pickup_time[0],
+            dropoff_time=self.dropoff_time if not isinstance(self.dropoff_time, list) else self.dropoff_time[0],
         )
+    
+    def create_param_dict(self) -> Dict[str, Any]:
+        return {
+            "n_robots": self.n_robots,
+            "n_tasks": self.n_tasks,
+            "speed": self.speed,
+            "seed": self.seed, # type: ignore
+            "arena_width": self.arena_width,
+            "arena_height": self.arena_height,
+            "interface_gap": self.interface_gap,
+            "task_dist_calc": self.task_dist_calc,
+            "task_distribution": self.task_distribution,
+            "robot_initial_placements": self.robot_initial_placements,
+            "allowed_to_switch": self.allowed_to_switch,
+            "gamma": self.gamma,
+            "k": self.k,
+            "m": self.m,
+            "switching_cost": self.switching_cost,
+            "delay_random_range": [self.delay_random_range],
+            "transfer_time": self.transfer_time,
+            "pickup_time": self.pickup_time,
+            "dropoff_time": self.dropoff_time
+        }
 
-    def __repr__(self) -> str:
+    def _optimal_concerns(self) -> str:
         return f"ModelParams({self.n_robots=},{self.n_tasks=}{self.speed=},{self.arena_width=},{self.arena_height=},{self.interface_gap=},{self.task_dist_calc=},{self.task_distribution=},{self.delay_random_range=},{self.transfer_time=},{self.pickup_time=},{self.dropoff_time=}"
 
     def __hash__(self) -> int:
         return hash(f"{self.n_robots}{self.n_tasks}{self.speed}{self.arena_width}{self.arena_height}{self.interface_gap}{self.task_dist_calc}{self.task_distribution}{self.robot_initial_placements}{self.delay_random_range}{self.transfer_time}{self.pickup_time}{self.dropoff_time}")
 
+    def is_singleton(self) -> bool:
+        return not ( isinstance(self.n_robots, list) or isinstance(self.n_tasks, list) or isinstance(self.speed, list) or isinstance(self.arena_width, list) or isinstance(self.arena_height, list) or isinstance(self.interface_gap, list) or isinstance(self.task_dist_calc, list) or isinstance(self.task_distribution, list) or isinstance(self.robot_initial_placements, list) or isinstance(self.allowed_to_switch, list) or isinstance(self.gamma, list) or isinstance(self.k, list) or isinstance(self.m, list) or isinstance(self.switching_cost, list) or isinstance(self.delay_random_range, list) or isinstance(self.transfer_time, list) or isinstance(self.pickup_time, list) or isinstance(self.dropoff_time, list) )
+
+
+@dataclass
+class RunResult(ModelParams):
+    RunId: Optional[int] = None
+    iteration: Optional[int] = None
+    Step: Optional[int] = None
+    total_deliveries: Optional[int] = None
+    total_crossings: Optional[int] = None
+    throughput: Optional[float] = None
+    allocation: Optional[List[np.ndarray]] = None
+    
+    optimal_delivery_count: Optional[float] = None
+    mae: Optional[float] = None
+    
+    def __post_init__(self):
+        self.creation_time = datetime.datetime.now() #to help ensure uniqueness between runs? we shall see
+
+    def save(self, filename: str):
+        with open(filename, "w+") as f:
+            f.write(f"{'\t'.join(v for k,v in self.__dict__.items() if k != "allocation")}\n")
+    
 def assignments(num_robots, num_tasks, depth=0):
     if num_robots == 0: raise ValueError("wtf")
     if num_tasks == 1: 
@@ -83,12 +135,12 @@ def find_optimal(params:ModelParams) -> Tuple[int, List[int]]:
     # print([x for x in cache.keys()][-1])
     # print(params.__repr__())
     
-    if params.__repr__() in cache: return cache[params.__repr__()] # pyright: ignore[reportReturnType, reportIndexIssue, reportCallIssue, reportArgumentType, reportOperatorIssue]
-    
+    if params._optimal_concerns() in cache: return cache[params._optimal_concerns()] # pyright: ignore[reportReturnType, reportIndexIssue, reportCallIssue, reportArgumentType, reportOperatorIssue]
     params = copy.deepcopy(params)
     params.allowed_to_switch = False
+    assert params.is_singleton, "cant find the optimal for a sweep"
     num_robots = params.n_robots
-    num_segments = params.n_tasks + 1
+    num_segments = params.n_tasks + 1 # pyright: ignore[reportOperatorIssue] the assertion catches it
     
     
     best_delivery_count = 0
@@ -96,28 +148,48 @@ def find_optimal(params:ModelParams) -> Tuple[int, List[int]]:
     for possible_assignment in tqdm.tqdm(assignments(num_robots, num_segments)):
         params.robot_initial_placements = np.array(possible_assignment)
         model = params.create_swarm_model()
-        model.run_for(1000)
+        model.run_for(3600)
         deliveries = model.delivery_log[-1]
         if deliveries > best_delivery_count:
             best_delivery_count = deliveries
             best_allocation = possible_assignment
     
-    cache[params] = (best_delivery_count, best_allocation) # type: ignore
+    cache[params._optimal_concerns()] = (best_delivery_count, best_allocation) # type: ignore
     with open("optimal.json", "r+") as f:
         f.write(jsonpickle.encode(cache)) # type: ignore
     
     return best_delivery_count, best_allocation # pyright: ignore[reportReturnType]
         
 
-def main():
-    params = ModelParams(task_dist_calc=70)
-    params.seed = int(np.random.randint(0, 100000))
-    dc, ba = find_optimal(params)
-    model = params.create_swarm_model()
-    model.run_for(1000)
-    deliveries = model.delivery_log[-1]
-    print(f"this trials efficiency was {deliveries/dc:.2%}")
+def run_and_save(params: ModelParams, filename: str, number_process:int = 1, itterations_per_combo:int=100, seeds: Optional[np.ndarray] = None, ):
+    if seeds is None:
+        seeds = np.random.randint(0, 100000, itterations_per_combo).tolist()
+    results = mesa.batch_run(
+        SwarmModel,
+        number_processes=number_process,
+        parameters=params.create_param_dict(),
+        max_steps=3600,
+        display_progress=True,
+        rng=seeds #pyright: ignore[reportArgumentType] THIS IS LITERALLY WHAT THE DOCS SAY TO DO https://mesa.readthedocs.io/latest/migration_guide.html#batch-run
+    )
+    for test in results:
+        test_result=RunResult(**{k:v for k,v in test.items() if "Segment" not in k})
+        oc, oa = find_optimal(test_result)
+        test_result.optimal_delivery_count = oc
+        assert test_result.allocation is not None, "Cleaner than type ignore"
+        total = 0.0
+        for allocation in test_result.allocation:
+            total += np.linalg.norm(allocation-oa)
+        test_result.mae = float(total / len(test_result.allocation)) / test_result.n_robots # pyright: ignore[reportOperatorIssue] #its an np floating type not a python float so convert it over
+        test_result.save(filename)
 
+def main():
+    params = ModelParams()
+    params.n_tasks = [2,3,4,5]
+    params.n_robots = [20,30,40,50] #let the model set it to 10*number of tasks
+    
+    
+    run_and_save(params, "results.tsv", 5)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
